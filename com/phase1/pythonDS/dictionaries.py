cbcf51ec8f298587f717b7dc1923d0b92fe407ad
mi2703 <mohammedibrahim2703@gmail.com>
2022-04-25 23:41:46 +0530

    Code for beginners.

--- com/phase1/pythonDS/dictionaries.py
+++ com/phase1/pythonDS/dictionaries.py
@@ -34,6 +34,11 @@ with open('D:\FAYAZ\DataScience\GL\Dataset\stock_price.csv','r') as f:
         price=float(token[1])
         stock_prices.append([day,price])
 
+### Nested Maps
+user_info = {
+             1: {'name': 'Jason', 'age': '17', 'gender': 'Male'},
+             2: {'name': 'Jessica', 'age': '30', 'gender': 'Female'}}
+
 if __name__ == '__main__':
    print(phone_book.get("faiz"))
    print(phone_book , "Not Sorted")
@@ -44,3 +49,8 @@ if __name__ == '__main__':
    print('\n'  " items :--" , phone_book.items())
    print('\n' "Length Of Variable ",len(phone_book))
    print('\n',myDict)
+   print(user_info[1]['gender'])
+   print(sorted(myDict.items()) , " Sorting-1") ## notworked
+   for i in sorted(phone_book_1):
+       print(i,phone_book_1[i],end= " ") ## Still not sorted
+
